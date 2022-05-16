afd42026c5289127fd9bef480736ef44272c92e5
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-05-16 11:24:01 -0500

    Update for FE

--- README.md
+++ README.md
@@ -9,11 +9,11 @@
 ## Directions
 
 Read the directions and complete all activities in each file, in this order:
-- `oop.rb`
+- `oop.js`
 - `self-evaluation.md`
 
 Based on what you know about Git workflow, make commits as you see fit.
 
 ## Submission
 
-After you've commited and pushed your work back up to your copy of this repo, provide the repo link in the Google Submission Form.
+After you've commited and pushed your work back up to your copy of this repo, provide the repo link in the Submission Form.
---
+++ oop.js
@@ -0,0 +1,33 @@
+// Write a class called Unicorn
+// it should have a dynamic name property
+// it should have a color property, that is silver by default
+// it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+
+
+
+// Write a class called Vampire
+// it should have a dynamic name property
+// it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
+// it should have a thirsty property, that is true by default
+// it should have a drink method. When called, the thirsty property changes to false
+
+
+
+//  Write a Dragon class
+// it should have a dynamic name property (string)
+//  it should have a dynamic rider property (string)
+//  it should have a dynamic color property (string)
+//  it should have a isHungry property that is true by default
+//  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+  // HINT: This requires you to use your knowledge from the conditionals lesson
+
+
+
+//  BIG CHALLNEGE: Write a Hobbit class
+//  it should have a dynamic name property (string)
+//  it should have a dynamic disposition property (string)
+//  it should have an age property that defaults to 0
+//  it should have a celebrateBirthday method. When called, the age increases by 1
+//  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
+//  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
+//  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
